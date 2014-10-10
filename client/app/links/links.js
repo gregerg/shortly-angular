c72913370e1a17f69067061e6df8839ea849faff
gregerg <gregerg@gmail.com>
2014-10-10 15:37:37 -0700

    added linksController functionality

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -1,5 +1,13 @@
 angular.module('shortly.links', [])
 
-.controller('LinksController', function ($scope, Links) {
+.controller('LinksController', function ($scope, $http, Links) {
   // Your code here
+  $scope.data = {};
+  $scope.getLinks = function(){
+    $http.get('/api/links')
+    .then(function(res) {
+      $scope.data.links = res.data;
+    })
+  };
+  $scope.getLinks();
 });
